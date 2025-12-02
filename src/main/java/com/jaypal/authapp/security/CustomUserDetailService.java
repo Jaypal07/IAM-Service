4d02b38aae10016da9987b84ed2fb0bc63d750c4
Jaypal Koli <kolijaypal47@gmail.com>
2025-12-02 17:23:34 +0530

    created securityFilterChain bean

---
+++ src/main/java/com/jaypal/authapp/security/CustomUserDetailService.java
@@ -0,0 +1,21 @@
+package com.jaypal.authapp.security;
+
+import com.jaypal.authapp.exceptions.ResourceNotFoundExceptions;
+import com.jaypal.authapp.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundExceptions("Invalid username or password"));
+    }
+}
