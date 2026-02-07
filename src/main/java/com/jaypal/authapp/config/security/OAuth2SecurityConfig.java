1de403d20cfa89416cc7a60bb83d75646568b102
Jaypal Koli <kolijaypal47@gmail.com>
2026-02-08 00:04:27 +0530

    added oauth2 api endpoint

--- src/main/java/com/jaypal/authapp/config/security/OAuth2SecurityConfig.java
+++ src/main/java/com/jaypal/authapp/config/security/OAuth2SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.jaypal.authapp.config.security;
 
-
 import com.jaypal.authapp.config.security.entrypoint.OAuth2AuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -17,33 +16,27 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 @Configuration
 @RequiredArgsConstructor
 @Order(1)
-@ConditionalOnProperty(
-        prefix = "spring.security.oauth2.client.registration.google",
-        name = "client-id"
-)
+@ConditionalOnProperty(prefix = "spring.security.oauth2.client.registration.google", name = "client-id")
 public class OAuth2SecurityConfig {
 
-    private final AuthenticationSuccessHandler successHandler;
-    private final AuthenticationFailureHandler failureHandler;
-    private final OAuth2AuthenticationEntryPoint authenticationEntryPoint;
+        private final AuthenticationSuccessHandler successHandler;
+        private final AuthenticationFailureHandler failureHandler;
+        private final OAuth2AuthenticationEntryPoint authenticationEntryPoint;
 
-    @Bean
-    public SecurityFilterChain oauthSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .securityMatcher("/oauth2/**", "/login/oauth2/**")
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
-                .authorizeHttpRequests(auth ->
-                        auth.anyRequest().authenticated()
-                )
-                .oauth2Login(oauth -> oauth
+        @Bean
+        public SecurityFilterChain oauthSecurityFilterChain(HttpSecurity http) throws Exception {
+                http
+                        .securityMatcher("/api/v1/oauth2/authorization/**",
+                                "/oauth2/authorization/**",
+                                "/login/oauth2/code/**")
+                        .csrf(AbstractHttpConfigurer::disable)
+                        .cors(Customizer.withDefaults())
+                        .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                        .oauth2Login(oauth -> oauth
                         .successHandler(successHandler)
-                        .failureHandler(failureHandler)
-                )
-                .exceptionHandling(ex ->
-                        ex.authenticationEntryPoint(authenticationEntryPoint)
-                );
+                        .failureHandler(failureHandler))
+                        .exceptionHandling(ex -> ex.authenticationEntryPoint(authenticationEntryPoint));
 
-        return http.build();
-    }
+                return http.build();
+        }
 }
